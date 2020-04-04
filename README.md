3011422aa11a59e3b1e59de0cca481db8e094b13
lmdsultan <mdgetbetter.gmail.com>
2020-04-04 21:28:53 +0300

    start frontend user

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 
 ![](https://media.giphy.com/media/xUNd9KyYGAILrRYAvu/giphy.gif)
- ### Installed packages
+ ## Installed packages
+ ### Backend
  npm i express express-validator bcryptjs config gravatar jsonwebtoken mongoose request   
  npm i -D nodemon concurrently
+ ### Frontend
+ npm i axios redux-router-dom redux react-redux redux-thunk redux-devtools-extension moment react-moment 
---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+export const Navbar = () => {
+    return (
+        <nav className="navbar bg-dark">
+        <h1>
+          <Link to="/"><i className="fas fa-code"></i>Connector</Link>
+        </h1>
+        <ul>
+          <li><a href="profiles.html">Profiles</a></li>
+          <li><Link to="/register">Register</Link></li>
+          <li><Link to="/login">Login</Link></li>
+        </ul>
+      </nav>
+    )
+}
+export default Navbar
